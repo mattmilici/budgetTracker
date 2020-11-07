ad6a79aa4f56201e349c335ce1006911d64fe79d
Matthew Milici <milici.m915@gmail.com>
2020-11-06 16:23:37 -0800

    changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+//cahnges
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/budget', {
         useNewUrlParser: true,
